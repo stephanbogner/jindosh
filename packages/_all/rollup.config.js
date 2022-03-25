486ea4f9342bb778f21dfcefa0c65cd32b46616a
Stephan Bogner <hello@stephanbogner.de>
2022-03-26 00:44:13 +0100

    Rollup for minified and normal build

--- packages/_all/rollup.config.js
+++ packages/_all/rollup.config.js
@@ -3,10 +3,19 @@ import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'index.mjs',
-  output: {
-    file: '../../dist/jindosh.min.js',
-    format: 'umd',
-    name: 'jindosh'
-  },
-  plugins: [nodeResolve(), terser()]
+  output: [
+    {
+      file: '../../dist/jindosh.js',
+      format: 'umd',
+      name: 'jindosh',
+      plugins: []
+    },
+    {
+      file: '../../dist/jindosh.min.js',
+      format: 'umd',
+      name: 'jindosh',
+      plugins: [terser()]
+    }
+  ],
+  plugins: [nodeResolve()]
 };
